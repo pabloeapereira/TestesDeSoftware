cef626f80209b4be49ec34af4c7db5f04c5b100a
Pablo Emanuel Alves Pereira <vovollice@hotmail.com>
2021-01-07 20:45:57 -0300

    Mock de objetos

---
+++ 01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Threading;
+using Features.Clientes;
+using MediatR;
+using Moq;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteServiceTests
+    {
+        readonly ClienteTestsBogusFixture ClienteTestsBogus;
+
+        public ClienteServiceTests(ClienteTestsBogusFixture clienteTestsFixture)
+        {
+            ClienteTestsBogus = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var cliente = ClienteTestsBogus.GerarClienteValido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediator = new Mock<IMediator>();
+            var clienteService = new ClienteService(clienteRepo.Object, mediator.Object);
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            // Assert
+            clienteRepo.Verify(r => r.Adicionar(cliente),Times.Once);
+            mediator.Verify(m => m.Publish(It.IsAny<INotification>(),default),Times.Once);
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            // Arrange
+           
+
+            
+
+            // Act
+            
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            // Arrange
+            
+
+
+            // Act
+           
+
+            // Assert 
+           
+        }
+    }
+}
